b6f18be7a5e3f83a3bf95b023759d5559d4892c3
cicadasmile <1327880701@qq.com>
2021-11-11 21:36:54 +0800

    butte-frame 组件二次浅封装

---
+++ frame-gateway/src/main/java/com/butte/gateway/config/GatewayConfig.java
@@ -0,0 +1,13 @@
+package com.butte.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 网关基础配置
+ * @author 公众号:知了一笑
+ * @since 2021-08-07 19:34
+ */
+@Configuration
+public class GatewayConfig {
+
+}
