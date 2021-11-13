25de451d79059572d837e32b28a21e1720cac7f7
cicadasmile <1327880701@qq.com>
2021-11-13 11:53:54 +0800

    butte-frame 组件二次浅封装

---
+++ frame-seata/src/main/java/com/butte/seata/config/SeataConfig.java
@@ -0,0 +1,13 @@
+package com.butte.seata.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Seata事务配置
+ * @author 公众号:知了一笑
+ * @since 2021-08-07 21:15
+ */
+@Configuration
+public class SeataConfig {
+
+}
