f0b01a49faee7ee7745b07d51b241e9b08aaebb4
cicadasmile <1327880701@qq.com>
2021-11-13 11:53:15 +0800

    butte-frame 组件二次浅封装

---
+++ frame-nacos/src/main/java/com/butte/nacos/config/NacosConfig.java
@@ -0,0 +1,13 @@
+package com.butte.nacos.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Nacos配置文件
+ * @author 公众号:知了一笑
+ * @since 2021-08-07 21:04
+ */
+@Configuration
+public class NacosConfig {
+
+}
