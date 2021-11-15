4a4bac5a90339f95432225de8952c8b178d3090c
cicadasmile <1327880701@qq.com>
2021-11-15 20:43:08 +0800

    butte-frame 组件二次浅封装

---
+++ frame-security/src/main/java/com/butte/security/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.butte.security.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Security安全配置
+ * @author 公众号:知了一笑
+ * @since 2021-09-02 22:42
+ */
+@Configuration
+public class SecurityConfig {
+
+}
