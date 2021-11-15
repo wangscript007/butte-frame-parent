97483e4ba0254a83424b927a7674f35227d498d0
cicadasmile <1327880701@qq.com>
2021-11-15 21:08:40 +0800

    butte-frame 组件二次浅封装

---
+++ frame-sleuth/src/main/java/com/butte/sleuth/config/SleuthConfig.java
@@ -0,0 +1,13 @@
+package com.butte.sleuth.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Sleuth配置
+ * @author 公众号:知了一笑
+ * @since 2021-09-19 11:17
+ */
+@Configuration
+public class SleuthConfig {
+
+}
