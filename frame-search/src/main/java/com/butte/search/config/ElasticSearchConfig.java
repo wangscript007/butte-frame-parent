90533ea432e669826da087aa445358f40299b554
cicadasmile <1327880701@qq.com>
2021-11-16 20:15:24 +0800

    butte-frame 组件二次浅封装

---
+++ frame-search/src/main/java/com/butte/search/config/ElasticSearchConfig.java
@@ -0,0 +1,14 @@
+package com.butte.search.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 基础配置
+ * @author 公众号:知了一笑
+ * @since 2021-08-07 18:12
+ */
+@Configuration
+public class ElasticSearchConfig {
+
+
+}
